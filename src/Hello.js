366e59e2a98375798f860a98b078e161022b4475
EnzoSt <Stellavatoe@gmail.com>
2022-05-11 19:34:44 +0200

    components-07

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { Message } from "./Message";
 
 export class Hello extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (<div>
+        <h1>Hello, World!</h1>
+        <Message />
+    </div>);
   }
 }
