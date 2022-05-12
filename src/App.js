58c1952549eea9c1ec26aeef05b5bbda3eebe265
EnzoSt <Stellavatoe@gmail.com>
2022-05-12 11:48:52 +0200

    conditional-rendering-01

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <Welcome name={"Leo"} age={18} />
+        <Welcome name={"Leo"} age={19} />
       </div>
     );
   }
