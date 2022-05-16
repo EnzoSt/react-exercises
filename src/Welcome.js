0d8782e68404cdc8c8be87370d937afeef80b206
EnzoSt <Stellavatoe@gmail.com>
2022-05-16 15:57:31 +0200

    fixed-props-02

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-    return <p>Welcome, {this.props.name}!</p>;
+    return <p>Welcome, {this.props.name ?? "User"}!</p>;
   }
 }
+
+// We can also set a default value using an object through ComponentName.defaultProps.
