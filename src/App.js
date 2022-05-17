147356e59985de09d046600ecb3e622f364f8b7d
EnzoSt <Stellavatoe@gmail.com>
2022-05-17 09:47:57 +0200

    events-03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
@@ -8,10 +9,11 @@ export class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello />
-        <Welcome name={"Leo"} age={18} />
         <Counter initialValue={0} incrementBy={1} />
+        <Hello />
         <ClickCounter />
+        <Welcome name={"Leo"} age={18} />
+        <ClickTracker />
       </div>
     );
   }
