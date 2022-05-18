c1811cb752871db9f24c81b175713262ea4db4e6
EnzoSt <Stellavatoe@gmail.com>
2022-05-18 11:14:01 +0200

    forms-05

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { ClickTracker } from "./ClickTracker";
 // import { Counter } from "./Counter";
 import { Hello } from "./Hello";
 import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
+import { UncontrolledLogin } from "./UncontrolledLogin";
+// import { Login } from "./Login";
 
 export class App extends React.Component {
   render() {
@@ -15,7 +16,8 @@ export class App extends React.Component {
         <InteractiveWelcome />
         <ClickCounter />
         <ClickTracker />
-        <Login />
+        {/* <Login /> */}
+        <UncontrolledLogin />
       </div>
     );
   }
