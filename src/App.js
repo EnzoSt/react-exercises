6cac1d4e69f6d2b1ab10a3ddef18f9a353e59eac
EnzoSt <Stellavatoe@gmail.com>
2022-05-31 01:11:14 +0200

    react-router-03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+//Add a new Route to the users/:username path that renders a ShowGithubUser component that receives the username as a path parameter and renders the GithubUser component
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
+import { GithubUser } from "./GithubUser";
 
 export function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Welcome name={"Enzo"} />} />
+        <Route path= "/:username" element={<GithubUser />} />
         <Route path="/counter" element={<Counter />} />
       </Routes>
     </div>
