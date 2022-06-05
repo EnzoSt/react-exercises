4aeddd82b442520f1493b36c8bbf507e14e8e503
EnzoSt <Stellavatoe@gmail.com>
2022-06-05 17:41:20 +0200

    redux-03

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
 // import { App } from "./App";
 import "./index.css";
-import { Root } from "./Root";
-import { incrementCounter } from "./state/CounterState";
+// import { Root } from "./Root";
+// import { incrementCounter } from "./state/CounterState";
 import { Store } from "./state/Store";
+import { addTodo, editTodo, removeTodo } from "./state/TodosState";
 
-const rootElement = document.querySelector("#root");
-const root = createRoot(rootElement);
+// const rootElement = document.querySelector("#root");
+// const root = createRoot(rootElement);
 
 // root.render(
 //   <StrictMode>
 //     <Root />
 //   </StrictMode>
 // );
-
 Store.subscribe(() => {
-  console.log(Store.getState())
-})
+  console.log(Store.getState());
+});
 
-Store.dispatch(incrementCounter(10))
+// Store.dispatch(incrementCounter(10));
+Store.dispatch(addTodo({ id: 1, title: "Do homework", completed: false }));
+Store.dispatch(editTodo(1, { completed: true }));
+Store.dispatch(addTodo({ id: 2, title: "Buy cheese", completed: false }));
+Store.dispatch(removeTodo(1))
--- src/state/Store.js
+++ src/state/Store.js
@@ -1,7 +1,9 @@
 import { createStore } from "redux";
-import { counterReducer } from "./CounterState";
+// import { counterReducer } from "./CounterState";
+import { todosReducer } from "./TodosState";
 
-export const Store = createStore(counterReducer)
+// export const Store = createStore(counterReducer)
+export const Store = createStore(todosReducer)
 
 
  
